96f04667be11d5b1609397a27e98284f37e98169
Dean Weesner <dwees@uber.com>
2016-12-30 21:46:02 -0800

    draw either star or color shift

--- shapes/starfield_shape.py
+++ shapes/starfield_shape.py
@@ -3,44 +3,53 @@ import pygame, math
 from random import (randint, randrange, random)
 from operator import add
 
-black = (0, 0, 0)
-white = (255, 255, 255)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+DENSITY = 0.05
+SPEED = 1
 
 class Star():
     def __init__(self, rect):
         direction = randrange(100000)
-        velmult = random() + 0.2
+        velmult = (random() + 0.2) * SPEED
         self.vel = [math.sin(direction) * velmult,
                     math.cos(direction) * velmult]
         self.rect = rect
-
         self.reset()
 
+        
     def move(self):
         self.pos = map(add, self.pos, self.vel)
 
+        
     def get_pos(self):
         return self.pos
 
+    
     def display(self, screen, color):
         pos = [int(self.pos[0]), int(self.pos[1])]
         screen.set_at(pos, color)
 
+        
     def reset(self):
         self.pos = [randint(self.rect.left, self.rect.right),
                     randint(self.rect.top, self.rect.bottom)]
 
+        
 class StarfieldShape(BaseShape):
     def __init__(self, screen, polygon):
         super(StarfieldShape, self).__init__(screen, polygon)
+        rect_area = polygon.get_rect().width * polygon.get_rect().height
+        num_stars = int(rect_area * DENSITY)
+        self.stars = [Star(self.polygon.get_rect()) for _ in range(num_stars)]
 
-        self.stars = [Star(self.polygon.get_rect()) for i in range(1000)]
         
     def draw(self):
         for star in self.stars:
-            star.display(self.screen, black)
+            star.display(self.screen, BLACK)
             star.move()
             if not self.polygon.contains_point(star.get_pos()):
                 star.reset()
             else:
-                star.display(self.screen, white)
+                star.display(self.screen, WHITE)
