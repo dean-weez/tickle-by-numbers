8b27dd165718178647dd5cf8716bf084cfe1fb06
Dean Weesner <dwees@uber.com>
2016-12-30 23:28:34 -0800

    move drawer into its own class

--- tbn.py
+++ tbn.py
@@ -1,40 +1,10 @@
-import sys, pygame
+import pygame
 from pygame.locals import *
 from shapes.starfield_shape import StarfieldShape
 from shapes.color_shift_shape import ColorShiftShape
+from drawer import Drawer
 from polygon import Polygon
 
-class Drawer():
-    def __init__(self, screen):
-        self.points = []
-        self.screen = screen
-        self.last_mouse_pos = (0,0)
-        
-    def add_point(self, pt):
-        self.points.append(pt)
-
-    def clear_points(self):
-        black = (0, 0, 0)
-        if len(self.points) > 1:
-            pygame.draw.lines(self.screen, black, False, self.points)
-        
-        self.points = []
-
-    def get_points(self):
-        return self.points
-
-    def draw(self):
-        white = (255, 255, 255)        
-        black = (0, 0, 0)
-        if len(self.points) > 0:
-            mouse_pos = pygame.mouse.get_pos()
-            pygame.draw.line(self.screen, black, self.points[-1], self.last_mouse_pos)
-            pygame.draw.line(self.screen, white, self.points[-1], mouse_pos)
-            self.last_mouse_pos = mouse_pos
-        if len(self.points) > 1:
-            pygame.draw.lines(self.screen, white, False, self.points)
-        
-            
 
 def main():
     pygame.init()
